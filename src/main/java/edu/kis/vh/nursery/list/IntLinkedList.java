698dadec9fbd7a53806a410f1558c4a52864a51e
vertenxx <vertenxx@gmail.com>
2022-02-11 21:48:47 +0100

    1.5.2 przenisienie klasy

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -39,3 +39,35 @@ public class IntLinkedList {
 	}
 
 }
+
+class Node {
+
+	private int value;
+	private Node prev;
+	private Node next;
+
+	public Node(int i) {
+		value = i;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+}
