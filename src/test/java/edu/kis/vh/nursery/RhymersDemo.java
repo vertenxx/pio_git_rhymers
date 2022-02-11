d38e51b000a77235905484d3e1078686111b0a11
vertenxx <vertenxx@gmail.com>
2022-02-11 20:50:48 +0100

    1.5.1 utworzenie metody testRhymers

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -7,7 +7,10 @@ class RhymersDemo {
 
 	public static void main(String[] args) {
 		Rhymersfactory factory = new DefaultRhymersFactory();
-		
+		testRhymers(factory);
+	}
+
+		private static void testRhymers(Rhymersfactory factory) {
 		DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
 				factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
 		
