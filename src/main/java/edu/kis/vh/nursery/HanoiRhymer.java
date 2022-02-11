124f5f0b561e147778121ff57a7f023c63d66f48
vertenxx <vertenxx@gmail.com>
2022-02-11 19:57:22 +0100

    1.4.1 zastapienie literalow

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,8 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-	int totalRejected = 0;
+	public static final int NO_REJECTIONS = 0;
+	int totalRejected = NO_REJECTIONS;
 
 	public int reportRejected() {
 		return totalRejected;
